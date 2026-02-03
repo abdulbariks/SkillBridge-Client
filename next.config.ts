faa32f5209098f904a5ffdc029b88d5a8ddc9842
abdulbariks <abdulbarik1997m@gmail.com>
2026-02-03 16:56:21 +0600

    NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_AUTH_API_URL}/api/auth/:path*`,
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
